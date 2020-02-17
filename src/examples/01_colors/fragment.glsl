ff4c30d20cc49a881cba3edb45aa74822a294a83
David (NoxWings) García Miguel <noxwings@gmail.com>
2020-02-17 22:06:58 +0100

    Exercise solutions

--- src/examples/01_colors/fragment.glsl
+++ src/examples/01_colors/fragment.glsl
@@ -11,15 +11,26 @@ void main() {
     vec3 color = vec3(0);
 
     // Example: Display uv coordinates
+    color = vec3(vCoordinates.x, vCoordinates.y, 0.0); // simple solution
+    color = vec3(vCoordinates, 0.0);                   // Using swizzling
 
     // Exercise 2: Display black and white a horizontal gradient
+    color = vec3(vCoordinates.x);                       // expands 1 value to all componnts xyz
 
     // Exercise 2b: Display a diagonal gradient
+    color = vec3((vCoordinates.x + vCoordinates.y) / 2.0);
 
     // Exercise 3: Make a "loading bar" driven by the uAnimation parameter
+    color = (vCoordinates.x < uAnimation) ? vec3(1, 0, 1) : vec3(0);
 
     // Exercise 4: Draw a circle
     // Exercise 4b: Why is the circle acually an ellipse? could you fix it?
+    float radius = uAnimation;
+    vec2 coords = vCoordinates * 2.0 - 1.0;
+    coords.x *= uResolution.x / uResolution.y; // 4b
+    color = (length(coords) < radius) ? vec3(1.0): vec3(0.0); // 4a if/ternary
+    color = vec3( 1.0 - step(radius, length(coords)) );       // 4a without branch
+    color = vec3( smoothstep(radius, uAnimation - 0.01, length(coords)) ); // 4a with antialiasing
 
     gl_FragColor = vec4(color, 1.0);
 }
--- src/examples/02_noise_gradient/fragment.glsl
+++ src/examples/02_noise_gradient/fragment.glsl
@@ -17,14 +17,16 @@ varying vec2 vCoordinates;
 // in any point in between we should see some percentage of the
 // of the noise moving from one color to the other
 float exercise1(float noise) {
-    return 0.0;
+    // (noise < uAnimation)? 1.0 : 0.0;
+    return step(noise, uAnimation);
 }
 
 // EXCERCISE 2
 // Same as the previous exercise but try to make things appear vertically
 // For example the part on the top or bottom is the first last to appear
 float exercise2(float noise) {
-    return exercise1(noise);
+    // ((noise * vCoordinates.y) < uAnimation)? 1.0 : 0.0;
+    return step(noise * vCoordinates.y, uAnimation);
 }
 
 // EXCERCISE 3
@@ -32,7 +34,9 @@ float exercise2(float noise) {
 // Right now the transition is a bit rough, try to use
 // `uBorderSmoothing` parameter to add some blending on the edges.
 float exercise3(float noise) {
-    return exercise2(noise);
+    float smoothing = max(uBorderSmoothing, 0.001);
+    float noiseV = noise * vCoordinates.y;
+    return smoothstep(noiseV, noiseV + smoothing, uAnimation * (1. + smoothing));
 }
 
 void main() {
